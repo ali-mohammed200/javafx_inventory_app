e8de558458ad9ce43946e9f2f013635bc3911c59
Mohammad Ali <ali.mohammed200@gmail.com>
2024-01-19 00:10:41 -0600

    Add part form main functionality

--- src/main/java/Controller/AddPartController.java
+++ src/main/java/Controller/AddPartController.java
@@ -1,20 +1,25 @@
 package Controller;
 
+import Model.InHouse;
+import Model.Inventory;
+import Model.Outsourced;
+import Model.Part;
 import com.c482_inventory_system.c482_pa.MainApplication;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Random;
 
 public class AddPartController implements Initializable {
     @FXML
@@ -37,11 +42,32 @@ public class AddPartController implements Initializable {
     @FXML
     private TextField customInput;
     @FXML
+    private Label customLabel;
+    @FXML
     private TextField minInput;
+    private ToggleGroup group = new ToggleGroup();
 
     @FXML
     protected void onSave(ActionEvent event) {
         String name = nameInput.getText();
+        int inv = Integer.parseInt(invInput.getText());
+        double price = Double.parseDouble(priceInput.getText());
+        int max = Integer.parseInt(maxInput.getText());
+        int min = Integer.parseInt(minInput.getText());
+        String custom = customInput.getText();
+        int id = new Random().nextInt(1000);
+        Part part = null;
+        if (ihButton.isSelected()) {
+            int machineId = Integer.parseInt(custom);
+            part = new InHouse(id, name, price, inv, max, min, machineId);
+        } else if (osButton.isSelected()) {
+            part = new Outsourced(id, name, price, inv, max, min, custom);
+        }
+        Inventory.addPart(part);
+//        Part part = new Outsourced(1, "Outsourced Part Tire", 20.00, 20, 1, 5, "FOX");
+//        pr.addAssociatedPart(part);
+//        Part part2 = new InHouse(2, "InHouse Part Bell", 10.00, 27, 1, 5, 2);
+
 
         System.out.print(ihButton.isSelected());
     }
@@ -58,8 +84,17 @@ public class AddPartController implements Initializable {
 
     @FXML
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        ToggleGroup group = new ToggleGroup();
         ihButton.setToggleGroup(group);
         osButton.setToggleGroup(group);
+        group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+                if (ihButton.isSelected()) {
+                    customLabel.setText("Machine ID");
+                } else if (osButton.isSelected()) {
+                    customLabel.setText("Company Name");
+                }
+            }
+        });
     }
 }
