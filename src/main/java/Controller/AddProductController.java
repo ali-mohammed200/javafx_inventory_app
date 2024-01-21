d1122845ab9332c35170a6d7362fbcb2a821b922
Mohammad Ali <ali.mohammed200@gmail.com>
2024-01-20 23:52:58 -0600

    Add Product search and initial table

--- src/main/java/Controller/AddProductController.java
+++ src/main/java/Controller/AddProductController.java
@@ -1,18 +1,86 @@
 package Controller;
 
+import Model.Inventory;
+import Model.Part;
 import com.c482_inventory_system.c482_pa.MainApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class AddProductController {
+public class AddProductController implements Initializable  {
     @FXML
     private Stage stage;
+
+    @FXML
+    private TextField idInput;
+    @FXML
+    private TextField nameInput;
+    @FXML
+    private TextField invInput;
+    @FXML
+    private TextField priceInput;
+    @FXML
+    private TextField maxInput;
+    @FXML
+    private TextField minInput;
+    @FXML
+    private TableView tablePart;
+    @FXML
+    private TableColumn cPartId;
+    @FXML
+    private TableColumn cPartName;
+    @FXML
+    private TableColumn cPartInvLvl;
+    @FXML
+    private TableColumn cPartPrice;
+    @FXML
+    private Button addPartButton;
+    @FXML
+    private TextField searchBarPart;
+    @FXML
+    private TableView tableAssocPart;
+    @FXML
+    private TableColumn aPartId;
+    @FXML
+    private TableColumn aPartName;
+    @FXML
+    private TableColumn aPartInvLvl;
+    @FXML
+    private TableColumn aPartPrice;
+    @FXML
+    private Button removeAssocPart;
+    @FXML
+    private Button saveButton;
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    protected void onSearchPart(KeyEvent event) {
+        String searchTerm = searchBarPart.getText();
+        boolean isNumeric = searchTerm.chars().allMatch(Character::isDigit);
+        if (isNumeric && searchTerm != "") {
+            Integer id = Integer.parseInt(searchTerm);
+            Part part = Inventory.lookupPart(id);
+            if (part != null) {
+                searchTerm = part.getName();
+            }
+        }
+        tablePart.setItems(Inventory.lookupPart(searchTerm));
+    }
 
     @FXML
     protected void onCancel(ActionEvent event) throws IOException {
@@ -24,4 +92,13 @@ public class AddProductController {
         stage.show();
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        tablePart.setItems(Inventory.getAllParts());
+
+        cPartId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        cPartName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        cPartInvLvl.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        cPartPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+    }
 }
